9a24c04ae13e124ff42659097a0e74c25a573ecb
Tanner <tanner.tannerlevine@gmail.com>
2017-08-12 05:42:00 +0000

    you can add animals now!

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,22 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+  
+  def new
+    @animal = Animal.new
+  end
+  
+  def create
+    Animal.create(animal_params)
+    redirect_to root_path
+  end
+  
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :sound)
+  end
+
+end
